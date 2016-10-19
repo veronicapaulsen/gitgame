a2e9a0558dcb3964ddd89ef03aa3ec1505d899a9
Veronica Paulsen <veronicapaulsen@me.com>
2016-10-18 22:35:27 -0700

    changed it so the server does the validation instead of js page, so that eventually the server can run git commands on user's computer

--- levelOne.js
+++ levelOne.js
@@ -2,12 +2,32 @@ var level_complete = false;
 
 function checkAnswer(){
     var answer = document.getElementById("ua").value;
-    if(answer === "git init"){
-	window.alert("You initiated your git repository! Good job! Now you can go to the next level.");
-	level_complete = true;
-	return true;
+    //if(answer === "git init"){
+	//window.alert("You initiated your git repository! Good job! Now you can go to the next level.");
+    httpPostAsync(answer);
+	//return true;
+	//}
+    //return false;
+}
+
+
+
+
+function httpPostAsync(answer_){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function(){
+	if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+	    level_complete = true;
+	    window.alert("You initiated your git repository! Good job! Now you can go to the next level.");
+	}else if(xmlHttp.readyState == 4 && xmlHttp.status == 400){
+            level_complete = false;
+            window.alert("Your command was wrong! Try again");
+	}
     }
-    return false;
+    encoded_answer = encodeURIComponent(answer_);
+    theUrl = "http://localhost:8080/command?cmd="+encoded_answer;
+    xmlHttp.open( "POST", theUrl, true );
+    xmlHttp.send(answer_);
 }
 
 function checkLevelComplete(){
